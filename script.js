370c597874885ba3231ec247054c852061f6b6a5
Alisha092 <ali1382.emp@gmail.com>
2023-10-12 20:01:13 +0330

    Ali

---
+++ script.js
@@ -0,0 +1,14 @@
+let someObj = {
+    FirstName: "Ali",
+    LastName: "Sheibani",
+    Age: "18",
+    PlaceofBirth: "Iran",
+    LivesIn: "Germany",
+    City: "Koln",
+    Profession: "Software Enginear",
+    Edjucation: "Master of Computer Scseince",
+    Languages: "DE  EN  FA"
+}
+
+console.table(someObj);
+console.dir(someObj);
